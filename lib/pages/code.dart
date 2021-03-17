a55e8a068ccb458578e2a040a786a75203fa36ec
mahesh jamdade <maheshmn121@gmail.com>
2021-03-17 23:28:53 +0530

    fix: save image locally

--- lib/pages/code.dart
+++ lib/pages/code.dart
@@ -1,50 +1,91 @@
+import 'dart:typed_data';
+import 'dart:html' as html;
+import 'dart:ui' as ui;
+import 'dart:js' as js;
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:highlight/languages/dart.dart';
 import 'package:flutter_highlight/themes/monokai-sublime.dart';
 import 'package:code_text_field/code_text_field.dart';
 
-class WindowBuilder extends StatelessWidget {
-  final double borderRadius = 5;
+class WindowBuilder extends StatefulWidget {
   @override
+  _WindowBuilderState createState() => _WindowBuilderState();
+}
+
+class _WindowBuilderState extends State<WindowBuilder> {
+  final double borderRadius = 5;
+  final key = GlobalKey();
+  void saveAs(List<int> bytes, String fileName) {
+    js.context.callMethod("saveAs", <Object>[
+      html.Blob(<List<int>>[bytes]),
+      fileName
+    ]);
+  }
+
+  void saveCode(double maxRatio) async {
+    await Future<Null>.delayed(Duration(milliseconds: 2000));
+    RenderRepaintBoundary boundary =
+        key.currentContext!.findRenderObject() as RenderRepaintBoundary;
+    ui.Image image = await boundary.toImage(pixelRatio: maxRatio);
+
+    ByteData? byteData = await image.toByteData(format: ui.ImageByteFormat.png);
+    Uint8List pngBytes = byteData!.buffer.asUint8List();
+    saveAs(pngBytes, "code.png");
+  }
+
+  late Uint8List bytes = Uint8List(0);
   Widget build(BuildContext context) {
     // TODO: implement build
     return Scaffold(
+      appBar: AppBar(
+        title: Text('Beautiful Code'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.download_rounded),
+            onPressed: () => saveCode(1.5),
+          )
+        ],
+      ),
       body: Center(
         child: SingleChildScrollView(
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Align(
-                alignment: Alignment.center,
-                child: Container(
-                  width: MediaQuery.of(context).size.width * 0.8,
-                  decoration: BoxDecoration(
-                    color: Colors.black,
-                    borderRadius: BorderRadius.circular(5),
-                    boxShadow: [
-                      BoxShadow(
-                          blurRadius: 10,
-                          spreadRadius: 5,
-                          color: Colors.grey,
-                          offset: Offset(4, 10)),
-                      BoxShadow(
-                          blurRadius: 20,
-                          spreadRadius: 10,
-                          color: Color.fromRGBO(206, 213, 222, 0.8))
-                    ],
+          child: RepaintBoundary(
+            key: key,
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Align(
+                  alignment: Alignment.center,
+                  child: Container(
+                    width: MediaQuery.of(context).size.width * 0.8,
+                    decoration: BoxDecoration(
+                      color: Colors.black,
+                      borderRadius: BorderRadius.circular(5),
+                      boxShadow: [
+                        BoxShadow(
+                            blurRadius: 10,
+                            spreadRadius: 5,
+                            color: Colors.grey,
+                            offset: Offset(4, 10)),
+                        BoxShadow(
+                            blurRadius: 20,
+                            spreadRadius: 10,
+                            color: Color.fromRGBO(206, 213, 222, 0.8))
+                      ],
+                    ),
+                    child: Column(
+                      children: [
+                        HeaderBuilder(
+                            // headerColor: Colors.grey,
+                            ),
+                        CodeEditor(),
+                      ],
+                    ),
                   ),
-                  child: Column(
-                    children: [
-                      HeaderBuilder(
-                          // headerColor: Colors.grey,
-                          ),
-                      CodeEditor(),
-                    ],
-                  ),
-                ),
-              )
-            ],
+                )
+              ],
+            ),
           ),
         ),
       ),
@@ -100,7 +141,7 @@ class CodeEditor extends StatefulWidget {
 
 class _CodeEditorState extends State<CodeEditor> {
   CodeController? _codeController;
-  final source = "void main() {\n    print(\"Hello, world!\");\n}";
+  final source = "";
 
   @override
   void initState() {
@@ -124,7 +165,7 @@ class _CodeEditorState extends State<CodeEditor> {
               bottomLeft: Radius.circular(5), bottomRight: Radius.circular(5))),
       padding: EdgeInsets.symmetric(horizontal: 8, vertical: 8),
       showLines: false,
-      textStyle: TextStyle(fontFamily: 'SourceCode', height: 1),
+      textStyle: TextStyle(height: 1.5),
     );
   }
 }
