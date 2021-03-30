76ad8501c98b0d33adf26620a8988398f54882f8
mahesh jamdade <maheshmn121@gmail.com>
2021-03-30 23:04:24 +0530

    fix: refactor code

--- lib/constants/strings.dart
+++ lib/constants/strings.dart
@@ -1,5 +1,48 @@
 /// app string constants  from the UI go here in this page
 /// e.g
 const String APP_TITLE = 'Beautiful Snippet';
-List<String> languages = ['Dart', 'Java', 'Typescript', 'Javascript'];
+List<String> languages = ['Dart'];
 List<String> themes = ['Gradient-Dark', 'An-Old-Hope', 'Xcode', 'Monokai'];
+
+const String sourceTemplate =
+    """// Copyright (c) 2019, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyBeautifulApp());
+
+class MyBeautifulApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Beautiful Snippet Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Container(),
+    );
+  }
+}
+""";
+
+List<String> keywords = [
+  'abstract',
+  'class',
+  'enum',
+  'extends',
+  'extension',
+  'external',
+  'factory',
+  'implements',
+  'get',
+  'mixin',
+  'native',
+  'operator',
+  'set',
+  'typedef',
+  'with',
+  'covariant'
+];
--- lib/pages/snippet.dart
+++ lib/pages/snippet.dart
@@ -16,44 +16,40 @@ class SnippetBuilder extends StatefulWidget {
   _SnippetBuilderState createState() => _SnippetBuilderState();
 }
 
-// TODO: Themes and Color Schemes supported
-/// theme: 'gradient-dark', //black
-/// theme: 'an-old-hope', //black
-/// theme: 'xcode', //white
-//
-
 class _SnippetBuilderState extends State<SnippetBuilder> {
+  // late SpecsModel specs;
   Widget build(BuildContext context) {
-    final specs = Provider.of<SpecsModel>(context, listen: false);
-    return Container(
-      alignment: Alignment.center,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(widget.borderRadius),
-        border: specs.hasBorder ? Border.all(color: specs.borderColor) : null,
-        boxShadow: [
-          BoxShadow(
-              blurRadius: 15,
-              spreadRadius: 8,
-              color: Colors.black.withOpacity(0.3),
-              offset: Offset(3, 4)),
-        ],
-      ),
-      child: Column(
-        children: [
-          HeaderBuilder(
-            headerColor: specs.snippetHeaderColor,
-          ),
-          CodeEditor(
-            backgroundColor:
-                specs.snippetBackgroundColor, //TODO: ADD BACKGROUND CODE COLOR
-            theme: specs.codeTheme.toLowerCase(),
-            language: specs.language.toLowerCase(),
-            source: specs.sourceCode,
-            onChange: (x) {
-              specs.sourceCode = x;
-            },
-          ),
-        ],
+    return Consumer<SpecsModel>(
+      builder: (_, specs, child) => Container(
+        alignment: Alignment.center,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(widget.borderRadius),
+          border: specs.hasBorder ? Border.all(color: specs.borderColor) : null,
+          boxShadow: [
+            BoxShadow(
+                blurRadius: 15,
+                spreadRadius: 8,
+                color: Colors.black.withOpacity(0.3),
+                offset: Offset(3, 4)),
+          ],
+        ),
+        child: Column(
+          children: [
+            HeaderBuilder(
+              headerColor: specs.snippetHeaderColor,
+            ),
+            CodeEditor(
+              backgroundColor: specs
+                  .snippetBackgroundColor, //TODO: ADD BACKGROUND CODE COLOR
+              theme: specs.codeTheme.toLowerCase(),
+              language: specs.language.toLowerCase(),
+              source: specs.sourceCode,
+              onChange: (x) {
+                specs.sourceCode = x;
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
@@ -122,20 +118,50 @@ class _CodeEditorState extends State<CodeEditor> {
   CodeController? _codeController;
 
   @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final Map<String, TextStyle> map = {};
+    keywords.forEach((e) {
+      map[e] = TextStyle(color: Colors.blue);
+    });
     _codeController = CodeController(
         text: widget.source,
         language: allLanguages[widget.language],
+        stringMap: map,
         patternMap: {
-          r"\B#[a-zA-Z0-9]+\b": TextStyle(color: Colors.red),
-          r"\B@[a-zA-Z0-9]+\b": TextStyle(
-            fontWeight: FontWeight.w800,
-            color: Colors.purple,
-          ),
-          r"\B![a-zA-Z0-9]+\b":
-              TextStyle(color: Colors.yellow, fontStyle: FontStyle.italic),
+          /// Widget color
+          r'[A-Z][a-z0-9]+\(.*\)': TextStyle(color: Colors.blue),
+
+          r'^\\w*\\b(library|import|part of|part|export)\\b':
+              TextStyle(color: Colors.purple),
+
+          /// storage type annotation
+          r'@[a-zA-Z]+': TextStyle(color: Colors.cyan),
+
+          /// constants and vars
+          r'(?<!\\$)\\b(true|false|null)\\b(?!\\$)': TextStyle(color: green),
+
+          /// function name,
+          r'\([a-z]+[A-Z]+\\w+\)+\(.*\)':
+              TextStyle(color: Colors.greenAccent.shade700),
+
+          r'\([A-Z]+[a-zA-Z0-9]+\\w+\)+\(.*\)': TextStyle(color: Colors.cyan),
+
+          /// import as
+          r'\\b(as|show|hide)\\b': TextStyle(color: red)
+
+          /// Widget color
         },
         theme: themeMap[widget.theme]);
+    if (!_codeController!.hasListeners) {
+      _codeController!
+          .addListener(() => widget.onChange!(_codeController!.text));
+    }
 
     return CodeField(
       controller: _codeController!,
@@ -145,7 +171,7 @@ class _CodeEditorState extends State<CodeEditor> {
             horizontal: padding_small,
           ) +
           EdgeInsets.only(bottom: padding_small),
-      onChange: (x) => widget.onChange!(x),
+      // onChange: (x) => widget.onChange!(x),
       lineNumberStyle: LineNumberStyle(margin: 25.0, width: 50),
       decoration: BoxDecoration(
           color: widget.backgroundColor,
